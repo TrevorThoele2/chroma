52cd0d28632f1b042d5a8fd4f2b2de95d76dc1ec
Trevor Thoele <t_thoele@yahoo.com>
2021-03-25 07:54:11 -0500

    Changes.

--- Chroma/DetailedException.cpp
+++ Chroma/DetailedException.cpp
@@ -3,18 +3,13 @@
 namespace Chroma
 {
     DetailedException::DetailedException(const std::string& message) :
-        message(message)
+        runtime_error(message)
     {}
 
     DetailedException::DetailedException(const std::string& message, NameValuePairs details) :
-        message(message), details(details)
+        runtime_error(message), details(details)
     {}
-
-    std::string DetailedException::Message() const
-    {
-        return message;
-    }
-
+    
     auto DetailedException::Details() const -> NameValuePairs
     {
         return details;
